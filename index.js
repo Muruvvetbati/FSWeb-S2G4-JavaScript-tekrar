df35c04cabe3b64fc67e3250d39b3c890ca38a7c
Muruvvetbati <77552361+Muruvvetbati@users.noreply.github.com>
2024-02-06 19:00:59 +0300

    görev 1,2 tamamlandı.

--- index.js
+++ index.js
@@ -50,8 +50,8 @@ function KareninAlani(kenaruzunlugu) {
 	4. Hesaplanan çemberin çevresi döndürülecektir.
 */
 
-function CemberinCevresi(/* kodlar buraya */) {
-  /* kodlar buraya */
+function CemberinCevresi(yaricap) {
+  return 2*pi*yaricap;
 }
 
 /* (Oto test yok) Yukarıdaki CemberinCevresi fonksiyonunu yarıçap = 5 vererek aşağıda çalıştırıp, sonucu konsolda gözlemleyin (console.log)  */
@@ -64,8 +64,8 @@ function CemberinCevresi(/* kodlar buraya */) {
 	4. Hesaplanan çemberin alanı döndürülecektir.
 */
 
-function CemberinAlani(/* kodlar buraya */) {
-  /* kodlar buraya */
+function CemberinAlani(yaricap) {
+ return pi* yaricap*yaricap;
 }
 
 /* (Oto test yok) Yukarıdaki CemberinAlani fonksiyonunu yarıçap = 15 vererek aşağıda çalıştırıp, sonucu konsolda gözlemleyin (console.log)  */
@@ -89,35 +89,63 @@ function CemberinAlani(/* kodlar buraya */) {
 
 /*  (oto test yok) sayilar dizisi içinde kaç adet sayı olduğunu konsola yazdırın */
 
-let ucetambolunenler,
-  enkucuk,
-  enbuyuk,
-  ucebolunenlerintoplami,
+let ucetambolunenler = [],
+  enkucuk = sayilar[0],
+  enbuyuk = sayilar[0], 
   besyuzdenkucuksayilar,
   siralisayilar,
   tekraredensayilar;
 
 // 3a çözümü
 
+
+for(i = 0; i<sayilar.length; i++){
+ if( sayilar[i]< enkucuk){
+  enkucuk = sayilar [i];
+}
+};
+ console.log(enkucuk);
+
+for(i = 0; i<sayilar.length; i++){
+  if( sayilar[i]> enbuyuk){
+   enbuyuk = sayilar [i];
+ }
+ };
+
+
+ console.log(enbuyuk);
+
 /* kodlar buraya */
 
 // 3b çözümü:
 
+
+sayilar.forEach(sayi => {
+  if(sayi %3 === 0) {
+    ucetambolunenler.push[sayi];
+  }
+});
+
+
 /* kodlar buraya */
 
 // 3c çözümü:
+let ucebolunenlerintoplami = ucetambolunenler.reduce((toplam, sayi) => toplam + sayi, 0);
 
-/* kodlar buraya */
 
 // 3d çözümü
 
+ besyuzdenkucuksayilar = sayilar.filter(sayi => sayi < 500);
 /* kodlar buraya */
 
 // 3e çözümü
+siralisayilar = besyuzdenkucuksayilar.sort((a,b)=> a-b);
 
 /* kodlar buraya */
 
 // 3f çözümü
+
+
 
 /* kodlar buraya */
 
